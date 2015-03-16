27ebdcabee7a029ef59fee575044e6a7214e00f8
vtch99 <crystal.tyan@gmail.com>
2015-03-16 07:20:08 +0000

    update

---
+++ SERVER/models/stars.js
@@ -0,0 +1,5 @@
+var db = require('../db')
+
+var Stars = function() {
+    
+}
--- SERVER/routes/admin.js
+++ SERVER/routes/admin.js
@@ -1,8 +1,38 @@
-var express = require('express')
-var router = express.Router()
+var express = require('express'),
+    router = express.Router(),
+    Admin = require('./modules/admin')
+
 
 router.get('/', function(req, res) {
   res.send('Welcome to Art-Daily server, but you can not do anything.')
+})
+
+router.get('/panel', function(req, res, next) {
+  if (req.session.admin) {
+    res.render('panel', {
+      admin: req.session.admin
+    })
+  } else res.redirect('/login.html')
+})
+
+
+router.post('/login', function(req, res, next) {
+  Admin.checkPasswd(req.body.admin_name, req.body.passwd, function(err, correct) {
+    if (correct) {
+      req.session.admin = Admin.get(req.body.admin_name, function(admin) {
+        req.session.admin = admin
+        res.send({success: 'welcome, login success!'})
+      })
+    } else {
+      res.send({error: 'admin_name or password error!'})
+    }
+  })
+})
+
+router.post('/logout', function(req, res, next) {
+  req.session.destroy(function(){
+    res.redirect('/login.html')
+  })
 })
 
 module.exports = router
